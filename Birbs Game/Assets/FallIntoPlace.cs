df1d3f4536693291daad84e5a473b9def6292266
Justin <justinwilkowski@gmail.com>
2017-11-21 19:17:36 -0800

    added bkacground and more bird animations
    added countdown and other UI sprites
    
    added level Scene and saved title scne
    
    added environment items and coded basic background movement

---
+++ Birbs Game/Assets/FallIntoPlace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallIntoPlace : MonoBehaviour {
+
+    float bottom;
+    float buffer = 3.9f; //because the below evidently isn't the "bottom"
+    void Start () {
+        bottom = Camera.main.WorldToViewportPoint(transform.position).y + buffer;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y <= bottom)
+        {
+            transform.position += new Vector3(0, .066f, 0);
+        }
+    }
+}
---
+++ Birbs Game/Assets/LevelCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCamera : MonoBehaviour {
+
+    public GameObject countdown;
+
+    bool exists = false;
+
+    float timer = 0;
+    float startCountdown = 3.0f;
+    float startMovement = 8.0f;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timer += Time.deltaTime;
+        if(timer >= startCountdown)
+        {
+            if (!exists)
+            {
+                Instantiate(countdown);
+                exists = true;
+            }
+        }
+        if (timer >= startMovement)
+        {
+            transform.position += new Vector3(.033f, 0, 0);
+        }
+        Debug.Log(timer);
+    }
+}
