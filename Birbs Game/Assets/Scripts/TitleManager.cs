df238107cca2515fc61f217618838924596c7e47
Justin <justinwilkowski@gmail.com>
2017-11-22 15:13:13 -0800

    got canvas buttons kind of working, need to reintroduce the background and make them actually go to correct scenes

---
+++ Birbs Game/Assets/Scripts/TitleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour {
+
+    public GameObject title;
+    public GameObject credits;
+    public GameObject exit;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
