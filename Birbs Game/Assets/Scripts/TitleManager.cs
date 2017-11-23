c68be923bfb1f099a7604615a8417c9df16a114c
Justin <justinwilkowski@gmail.com>
2017-11-22 17:37:42 -0800

    Main screen now looks as intended, buttons still nonfunctional for some reason

--- Birbs Game/Assets/Scripts/TitleManager.cs
+++ Birbs Game/Assets/Scripts/TitleManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
 
-    public GameObject title;
-    public GameObject credits;
-    public GameObject exit;
 
     void Start () {
 		
 	}
 	
 	void Update () {
-		
+        Debug.Log("why no work");
 	}
+
+    public void exitGame()
+    {
+        Debug.Log("exiting");
+        //Application.Quit();
+    }
+
+    public void playLevel()
+    {
+        Debug.Log("going to play scene");
+        //SceneManager.LoadScene("LevelScene");
+    }
+
+    public void goToCredits()
+    {
+        Debug.Log("going to credits");
+        //SceneManager.LoadScene("LevelScene");
+    }
 }
